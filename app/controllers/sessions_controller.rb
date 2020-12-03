45b06c1b3747812760ba695a170dcb6dc7d0f980
youshamahmood <youshamahmood96@gmail.com>
2020-12-03 17:28:20 +0600

    randm

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,28 @@
+class SessionsController < ApplicationController
+    before_action :logged_in_redirect, only: [:new, :create]
+    def new
+    end
+    def create
+        user = User.find_by(username: params[:session][:username])
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:success] = 'Logged In'
+            redirect_to root_path
+        else
+            flash.now[:error] = 'Wrong Credentials'
+            render 'new'
+        end
+    end
+    def destroy
+        session[:user_id] = nil
+        flash[:success] = "Logged Out"
+        redirect_to login_path
+    end
+    private
+    def logged_in_redirect
+        if logged_in?
+            flash[:error] = 'You are already logged in!'
+            redirect_to root_path
+        end
+    end  
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# User.create(username: 'Yousha' , password: 'password')
+# User.create(username: 'Yousha1' , password: 'password1')
+# User.create(username: 'Yousha2' , password: 'password2')
+# User.create(username: 'Yousha3' , password: 'password3')
+# User.create(username: 'Yousha4' , password: 'password4')
+
+# Message.create(body: "Hi", user: User.first)
+# Message.create(body: "What", user: User.last)
+# Message.create(body: "Are", user: User.first)
+# Message.create(body: "You doing", user: User.last)
